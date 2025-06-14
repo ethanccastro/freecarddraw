4ef74d1a4d1902b9e58bcafa5273fbc5f42c40b3
Ethan Blake Castro <ethanccastro@gmail.com>
2025-06-14 00:29:26 -0700

    adding deployment debugging steps

--- scripts/update-security-group.sh
+++ scripts/update-security-group.sh
@@ -3,11 +3,11 @@
 # Get GitHub Actions IP ranges
 GITHUB_ACTIONS_IPS=$(curl -s https://api.github.com/meta | jq -r '.actions[]')
 
-# Update security group (replace sg-xxxxxxxx with your security group ID)
+# Update security group
 for ip in $GITHUB_ACTIONS_IPS; do
     aws ec2 authorize-security-group-ingress \
-        --group-id sg-xxxxxxxx \
+        --group-id sg-04489da67bf57d42e \
         --protocol tcp \
         --port 22 \
-        --cidr $ip/32
+        --cidr "$ip"
 done 
